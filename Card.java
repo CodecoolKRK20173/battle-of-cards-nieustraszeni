9e720f5523beb913da749c964224e42cc8f1374f
Paweł Stój <routi1992@gmail.com>
2018-04-24 14:11:41 +0200

    add some empty line to refactoring

--- Card.java
+++ Card.java
@@ -6,6 +6,7 @@ public class Card {
     // private String status;
     private int color;
 
+    
     public Card(String suit, int rank, boolean faceDown, int color){
         this.suit = suit;
         this.rank = rank;
@@ -13,33 +14,41 @@ public class Card {
         this.faceDown = faceDown;
     }
 
+
     public String getSuit(){
         return suit;
     }
     
+
     public int getRank(){
         return rank;
     }
+
 
     public boolean isFaceDown(){
         return faceDown;
     }
 
+
     public String getStatus(){
         return status;
     }
+
 
     public int getColor(){
         return color;
     }
 
+
     public String toString(){
         return "The " + "Rank" + rank + " of " + "Suit" + suit;
     }
+
     
     public void flipCard(){
         // check faceDown status and change ascii outfit from file  
     }
+
 
     public boolean compareTo(){
         // compare rank cards from two players and return boolean
