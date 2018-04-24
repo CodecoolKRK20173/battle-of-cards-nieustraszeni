4ed6f5262c9f80db999141f1855640080c15a77f
Paweł Stój <routi1992@gmail.com>
2018-04-24 10:48:39 +0200

    add some variables and getters for them

---
+++ Card.java
@@ -0,0 +1,29 @@
+
+public class Card {
+    private String suit;
+    private int rank;
+    private boolean faceDown;
+    private String status;
+    private int color;
+
+    public String getSuit(){
+        return suit;
+    }
+    
+    public int getRank(){
+        return rank;
+    }
+
+    public boolean isFaceDown(){
+        return faceDown;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public int getColor(){
+        return color;
+    }
+    
+}
