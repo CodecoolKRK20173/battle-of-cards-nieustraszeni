68705b1fd0f19dc2da4935139a7a771f5c8707a2
Czakero <bartosz.pyrz96@gmail.com>
2018-04-25 12:18:33 +0200

    change constructor name, play method deleted

--- Human.java
+++ Human.java
@@ -3,17 +3,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Human extends Player {
-    public String name;
+    private String name;
 
-    Player(String name) {
+    Human(String name) {
         this.name = name;
     }
-
-
-    public void play() {
-        System.out.println(name + " is playing...");
-    }
-
 
     public String getName() {
         return name;
