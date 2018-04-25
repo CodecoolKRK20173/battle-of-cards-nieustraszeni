1710ec2a34ef78bc8389dcdb5c04bbbe501c79e2
michaal111 <michalgrzegorczyk19@gmail.com>
2018-04-25 12:05:07 +0200

    add fields and getters

--- SuitType.java
+++ SuitType.java
@@ -1,3 +1,18 @@
 public enum SuitType {
+    HEARTS(1, "hearts"),
+    DIAMONDS(2, "diamonds"),
+    SPADES(3, "spades"),
+    CLUBS(4, "clubs");
     
+    private int suitNum;
+    private String suitName;
+
+
+    public int getSuitNumber() {
+        return this.suitNum;
+    }
+
+    public String getSuitName() {
+        return this.suitName;
+    }
 }
