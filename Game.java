e2723841f1933289c08a3cbb9f31c1c3953b8f6b
Czakero <bartosz.pyrz96@gmail.com>
2018-04-25 12:17:55 +0200

    dealCards method changed, createDeckForPlayer method added

--- Game.java
+++ Game.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 public class Game {
     private Player player1 = new Player();
     private Player player2 = new Player();
-    private Deck deck = new Deck();
-    private List<Card> startDeck = new ArrayList<>();
     private List<Card> player1HandDeck = new ArrayList<>();
     private List<Card> player2HandDeck = new ArrayList<>();
     private List<Card> player1WinPotDeck = new ArrayList<>();
@@ -13,26 +11,34 @@ public class Game {
     private List<Card> battleField = new ArrayList<>();
 
 
-    public void dealCards(Player player1, Player player2){
-        startDeck = deck.createNewDeck();
-        player1HandDeck = deck.createDeckForPlayer(startDeck);
-        player2HandDeck = deck.createDeckForPlayer(startDeck);
+    public void dealCards(Player player1, Player player2) {
+        player1HandDeck = createDeckForPlayer(startDeck);
+        player2HandDeck = createDeckForPlayer(startDeck);
+    }
+    
+    public List<Card> createDeckForPlayer() {
+        for(int i = HALF_SIZE_OF_DECK; i > 0; i--) {
+            playerHandDeck.add(Card.getCards().get(i));
+            Card.getCards().remove(i);
+        }
+
+        return playerHandDeck;
     }
     
         
-    public boolean isGameWon(){
+    public boolean isGameWon() {
         // create conditions to win game
 
         return true;
     }
 
 
-    public void roundOfGame(){
+    public void roundOfGame() {
         // create conditions to start round
         // check here method evaluableRound
     }
 
-    public void dragCardToBattleField(){
+    public void dragCardToBattleField() {
         int firstElement = 0;
 
         battleField.add(player1HandDeck.get(firstElement));
@@ -43,33 +49,33 @@ public class Game {
     }
 
 
-    public void dragCardWhenDraw(){
+    public void dragCardWhenDraw() {
         System.out.println("Round draw");
         for (int i = 0; i < 2; i++)
             dragCardToBattleField();
     }
 
 
-    public void runFightMode(){
+    public void runFightMode() {
         while (battleField.size() != 0){
             int lastCardIndex = battleField.size() - 1;
             int beforeLastCardIndex = battleField.size() - 2;
 
             if (battleField.size() == 2){
-                if (battleField.get(0).getRank() > battleField.get(1).getRank()){
+                if (battleField.get(0).getRank() > battleField.get(1).getRank()) {
                     playerWinRound(player1HandDeck);
                     
-                } else if (battleField.get(0).getRank() < battleField.get(1).getRank()){
+                } else if (battleField.get(0).getRank() < battleField.get(1).getRank()) {
                     playerWinRound(player2HandDeck);
 
                 } else {    
                     dragCardWhenDraw();
 
                 }
-            } else{
-                if (battleField.get(beforeLastCardIndex).getRank() > battleField.get(lastCardIndex).getRank()){
+            } else {
+                if (battleField.get(beforeLastCardIndex).getRank() > battleField.get(lastCardIndex).getRank()) {
                     playerWinRound(player1HandDeck);
-                } else if (battleField.get(beforeLastCardIndex).getRank() < battleField.get(lastCardIndex).getRank()){
+                } else if (battleField.get(beforeLastCardIndex).getRank() < battleField.get(lastCardIndex).getRank()) {
                     playerWinRound(player2HandDeck);
                 } else {
                     dragCardWhenDraw();
@@ -79,26 +85,26 @@ public class Game {
     }
 
 
-    public void playerWinRound(List<Card> potDeck){
+    public void playerWinRound(List<Card> potDeck) {
 
-        for (int i = 0; i < battleField.size(); i++){
+        for (int i = 0; i < battleField.size(); i++) {
             potDeck.add(battleField.get(i));
         }
         battleField.clear();
     }
 
 
-    public void evaluableRound(){
+    public void evaluableRound() {
         // create conditions to win round or fight
     }
 
 
-    public void playGame(){
+    public void playGame() {
         boolean gameRun = true;
 
         while (gameRun){
-            if (player1HandDeck.size() == 0 || player2HandDeck.size() == 0){
-                if (isGameWon()){
+            if (player1HandDeck.size() == 0 || player2HandDeck.size() == 0) {
+                if (isGameWon()) {
                     System.out.println("You win the game!");
                     gameRun = false;
                 } else {
