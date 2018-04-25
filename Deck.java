cc5bff99efa4faa96e3a76838d510d4be826ddb8
Czakero <bartosz.pyrz96@gmail.com>
2018-04-25 11:09:54 +0200

    create deck method changed, createDeckForPlayer method changed

--- Deck.java
+++ Deck.java
@@ -1,34 +1,35 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private List<Card> deck = new ArrayList<>();
     private List<Card> playerHandDeck = new ArrayList<>();
     private final int HALF_SIZE_OF_DECK = 12;
-    private static final String RED = "red";
-    private static final String BLACK = "black";
+    private static final int RED = 1;
+    private static final int BLACK = 2;
+    private static final int SPADES = 1;
+    private static final int HEARTS = 2;
+    private Card card;
 
 
-    public List<Card> createNewDeck(){
+    public void createNewDeck() {
         for (int suit = 1; suit < 5; suit++){
             for (int rank = 1; rank < 7; rank++){
-                if (suit == 1 || suit == 2)
-                    deck.add(new Card(suit, rank, RED));
+                if (suit == SPADES || suit == HEARTS)
+                    card = new Card(suit, rank, RED);
                 else
-                    deck.add(new Card(suit, rank, BLACK));
+                    card = new Card(suit, rank, BLACK);
             }
         }
 
-        Collection.shuffle(deck);
-        return deck;
+        Collections.shuffle(Card.getCards());
     }
 
 
-    public List<Card> createDeckForPlayer(List<Card> deck){
+    public List<Card> createDeckForPlayer() {
         for(int i = HALF_SIZE_OF_DECK; i > 0; i--){
-            playerHandDeck.add(deck(i));
-            deck.remove(i);
+            playerHandDeck.add(Card.getCards().get(i));
+            Card.getCards().remove(i);
         }
 
         return playerHandDeck;
