d95ced4deb4a2c65c66a7d3e31eb76bb04dde078
michaal111 <michalgrzegorczyk19@gmail.com>
2018-04-25 09:42:29 +0200

    implement Human class

---
+++ Human.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Human extends Player {
+    private List<Card> handDeck;
+    private List<Card> winPotDeck;
+    public String name;
+
+    Player(String name) {
+        this.name = name;
+        handDeck = new ArrayList<>();
+        winPotDeck = new ArrayList<>();
+    }
+
+    public void play() {
+        System.out.println(name + " is playing...");
+    }
+
+
+    public List<Card> getHandDeck() {
+        return handDeck;
+    }
+
+
+    public List<Card> getWinPotDeck() {
+        return winPotDeck;
+    }
+}
