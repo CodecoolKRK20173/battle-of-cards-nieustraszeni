5673de90c6911b489e6aeb2bf55d5fb6ac77c32e
Czakero <bartosz.pyrz96@gmail.com>
2018-04-25 12:18:52 +0200

    play method deleted

--- Player.java
+++ Player.java
@@ -3,9 +3,6 @@ public abstract class Player {
     private List<Card> handDeck;
     private List<Card> winPotDeck;
 
-    abstract void play();
-
-    
     public List<Card> getHandDeck() {
         return handDeck;
     }
